42f594b4dc467eadade1875fb4914e9532625165
Davi William Moraes Suga <daviciencia1@gmail.com>
2019-01-19 21:59:22 -0200

    added automatic read and reply messages

--- whatsappy.py
+++ whatsappy.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import os
 
 driver = webdriver.Chrome()
 driver.get("https://web.whatsapp.com")
@@ -6,8 +7,25 @@ name = "Whatsapp Bot"
 input('Enter any key when you scan the qr code')
 user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
 user.click()
-msgBox = driver.find_element_by_xpath("//div[@spellcheck='true']")
 
-msgBox.send_keys('Hello Selenium')
-button = driver.find_element_by_xpath('//span[@data-icon="send"]')
-button.click()
+def sendText(msg, driver):
+    msgBox = driver.find_element_by_xpath("//div[@spellcheck='true']")
+    msgBox.send_keys(msg)
+    button = driver.find_element_by_xpath('//span[@data-icon="send"]')
+    button.click()
+
+msgSent=driver.find_elements_by_class_name('_3zb-j')
+lastMsg = msgSent[-1].text
+print(lastMsg)
+executing=True
+while executing:
+    if lastMsg=='/ping google':
+        os.system('ping google.com')
+        sendText('Pingando google', driver)
+    elif '/command' in lastMsg:
+        os.system(lastMsg[8:])
+        sendText('Executando comando {}'.format(lastMsg[8:]), driver)
+    elif lastMsg=='/quit':
+        sendText('Quitting...', driver)
+    else:
+        lastMsg=msgSent[-1].text
